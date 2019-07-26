7c4bfd3286f19b75607055afdc54bca8e7231c92
Jared Burnham <zlolris97@gmail.com>
2019-07-26 02:41:09 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,78 @@
-def consolidate_cart(cart)
-  # code here
+require "pry"
+def consolidate_cart(cart) 
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart 
 end
+  
 
-def apply_coupons(cart, coupons)
-  # code here
+
+def apply_coupons(cart, coupons) 
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += coupon[:num] 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost] / coupon[:num], 
+          :clearance => cart[name][:clearance], :count => coupon[:num]} 
+        end 
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, attribute|
+    if attribute[:clearance]
+      attribute[:price] = (attribute[:price] *= 0.80).round(2)
+      
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
   # code here
+  total = 0
+  cart = consolidate_cart(cart)
+  
+  if cart.length == 1
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    if cart_clearance.length > 1
+      cart_clearance.each do |item, data|
+        if data[:count] >=1
+          total += (details[:price]*details[:count])
+        end
+      end
+    else
+      cart_clearance.each do |item, details|
+        total += (details[:price]*details[:count])
+      end
+    end
+  else
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    cart_clearance.each do |item, details|
+      total += (details[:price]*details[:count])
+    end
+  end
+  
+
+  if total > 100
+    total = total*(0.90)
+  end
+  total
+
+
 end
